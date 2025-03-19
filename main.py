53fabd4acb9300f7f378a4e915ec55d293fb92ab
ianpoland <ianpoland96@gmail.com>
2025-03-19 18:08:10 -0400

    added word count function

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("greetings boots")
+from stats import count_words
+def get_book_text(filepath):
+    #print("here")
+    with open(filepath) as f:
+        file_content = f.read()
+        #print(file_content)
+    return file_content
+def main():
+    #print("here")
+    book_content=get_book_text("books/frankenstein.txt")
+    num_a = count_words(book_content)
+    print(f"{num_a} words found in the document")
+    #print(book_content)
+
+
+main()
---
+++ stats.py
@@ -0,0 +1,4 @@
+def count_words(book):
+    words = book.split()
+    num_words = len(words)
+    return num_words
