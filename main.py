abe888f0cf1311ffeb7f142c2867135f29eaf396
ianpoland <ianpoland96@gmail.com>
2025-03-20 19:54:19 -0400

    a basic program that takes in a book as a .txt file and returns total word count and the letters utilized in the book in decending order of frequency

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import count_words
+from stats import *
+import sys
 def get_book_text(filepath):
     #print("here")
     with open(filepath) as f:
@@ -6,11 +7,23 @@ def get_book_text(filepath):
         #print(file_content)
     return file_content
 def main():
-    #print("here")
-    book_content=get_book_text("books/frankenstein.txt")
+    if(len(sys.argv) < 2):
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    book_content=get_book_text(book_path)
     num_a = count_words(book_content)
-    print(f"{num_a} words found in the document")
-    #print(book_content)
-
+    char_dict = count_chars(book_content)
+    #print(char_dict)
+    sorted_repo = report(char_dict)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at books/frankenstein.txt")
+    print("----------- Word Count ----------")
+    print(f"Found {num_a} total words")
+    print("--------- Character Count -------")
+    for key in sorted_repo.keys():
+        print(f"{key}: {sorted_repo[key]}")
+    #print(sorted_repo)
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,22 @@ def count_words(book):
     words = book.split()
     num_words = len(words)
     return num_words
+def count_chars(book):
+    char_dict = dict()
+    book_as_list = list(book)
+    for character in book_as_list:
+        chara = character.lower()
+        if chara in char_dict.keys():
+            char_dict[chara]+=1
+        else:
+            char_dict[chara] = 1
+
+    return char_dict
+
+def report(char_dict):
+    new_dict = dict()
+    for key in char_dict.keys():
+        if(key.isalpha()):
+            new_dict[key] = char_dict[key]
+    sorted_dict = dict(sorted(new_dict.items(), key = lambda item : item[1], reverse = True))
+    return(sorted_dict)
